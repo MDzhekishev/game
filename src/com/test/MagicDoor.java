a7508ef4e445ed2cd7281faa272000cf6dff7e58
Maksat <dzhekishev@gmail.com>
2019-11-09 22:11:56 +0600

    return

--- src/com/test/MagicDoor.java
+++ src/com/test/MagicDoor.java
@@ -4,10 +4,18 @@ public class MagicDoor {
 
 
     public Hero[] getHeroes() {
-        Hero Warrior = new Hero(250, 20, "CriticalDamage");
-        Hero Mag = new Hero(250, 20, "Freeze");
-        Hero Healer = new Hero(200, 0, "MassHeal");
-        Hero Kinetic = new Hero(250, 20, "AttackOff");
-        return new Hero[3];
+
+        Hero [] heroes = new Hero[4];
+        Hero Warrior = new Hero(250,20,"CriticalDamage");
+        heroes [0]= Warrior;
+        Hero Mag = new Hero(250,20,"Freeze");
+        heroes [1]= Mag;
+        Hero Kinetic = new Hero(250,20,"AttackOff");
+        heroes [2]= Kinetic;
+        Hero Healer = new Hero(200,0,"MassHeal");
+        heroes [3]=Healer;
+        return heroes;
+        
     }
+
 }
